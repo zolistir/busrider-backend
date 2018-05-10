3b99ff05f296c0cdbc8c47c21435456494d2b8e8
Zoltan Stir <zolistir@gmail.com>
2018-05-10 20:41:14 +0300

    Fixed a decode

--- util/decode.js
+++ util/decode.js
@@ -1,7 +1,7 @@
 var decode = function(str) {
     var myStr = str;
-    var toBeDecoded = ['&#x21B;', '&#x103;', '&#x219;', '&#xE2;', '&#x218;', '&#x163;', '&#x15F;', '&#x103;', '&#x15E;'];
-    var toBeReplacedWith = ['ț', 'ă', 'ș', 'â', 'ș', 'ț', 'ș', 'ă', 'Ș'];
+    var toBeDecoded = ['&#x21B;', '&#x103;', '&#x219;', '&#xE2;', '&#x218;', '&#x163;', '&#x15F;', '&#x103;', '&#x15E;', '&#x2013;'];
+    var toBeReplacedWith = ['ț', 'ă', 'ș', 'â', 'ș', 'ț', 'ș', 'ă', 'Ș', '-'];
 
     for (i = 0; i <= toBeDecoded.length; i++) {
         myStr = myStr.replace(toBeDecoded[i], toBeReplacedWith[i]);
